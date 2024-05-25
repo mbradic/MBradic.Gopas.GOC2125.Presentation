bdc8020706a0eb573b1b6a6dffa38e9fecb5783c
Miroslav Bradic <mbradic@yahoo.com>
2024-05-25 02:29:29 +0200

    /overview/class

---
+++ MBradic.Gopas.GOC2125.Presentation/Circle.cs
@@ -0,0 +1,10 @@
+﻿class Circle {
+    public static double PI = 3.14;
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+    public double Radius { get; init; }
+    public double Area => PI * Radius * Radius;
+}
+
--- MBradic.Gopas.GOC2125.Presentation/Program.cs
+++ MBradic.Gopas.GOC2125.Presentation/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program {
+    public static void Main(string[] args) {
+        Circle small = new(5);
+        Circle big = new(25);
+        PrintCircleInfo(small, big);
+
+        Circle.PI = 4;
+        PrintCircleInfo(small, big);
+    }
+    private static void PrintCircleInfo(Circle small, Circle big) {
+        Console.WriteLine($"PI = {Circle.PI}");
+        Console.WriteLine($"small.Area = {small.Area}");
+        Console.WriteLine($"big.Area = {big.Area}");
+        Console.WriteLine();
+    }
+}
